c3b54e6f7d4d6dd0d33cc412e61f6884b8515574
mektebi <mhdmaktabi@gmail.com>
2020-06-12 04:29:38 +0300

    Initial commit

---
+++ load_category_list.py
@@ -0,0 +1,28 @@
+from locust import HttpLocust ,TaskSet ,task
+import json
+import requests
+import urllib3
+
+
+urllib3.disable_warnings()
+
+class UserBehaviour(TaskSet) :
+    requestHeader = "uulbfv3bozwp0wbvyax39o0348elq21f"
+    token = ""
+    refresh_token = ""
+
+    def on_start(self) :
+        self.access_token = 'uulbfv3bozwp0wbvyax39o0348elq21f'
+
+    @task(1)
+    def get_categories(self):
+        response = self.client.get('/rest/V1/categories',  headers={'Authorization': 'Bearer {}'.format(self.access_token)})
+        print(response.content)
+
+
+
+class User(HttpLocust) :
+    task_set = UserBehaviour
+    min_wait = 30000
+    max_wait = 70000
+    host = 'https://m2.leanscale.com'
---
+++ run.bat
@@ -0,0 +1,1 @@
+locust -f load_category_list.py  -c 2 -r 1 --no-web --csv=report -t 1m
